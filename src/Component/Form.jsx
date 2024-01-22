bc02ed281b28cda63a43efd04203f1a98bf40fff
Le-Yo <deoleyo@gmail.com>
2024-01-22 10:46:09 -0600

    localhost

--- src/Component/Form.jsx
+++ src/Component/Form.jsx
@@ -44,8 +44,8 @@ function Form() {
          
           localStorage.setItem('submittedData', JSON.stringify(formData));
 
-      // const response = await fetch('http://localhost:3001/submit-form', {
-      const response = await fetch('https://subcontract-backend-97436e1451f2.herokuapp.com/', {
+      
+      const response = await fetch('http://localhost:3001','https://subcontract-backend-97436e1451f2.herokuapp.com/', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
